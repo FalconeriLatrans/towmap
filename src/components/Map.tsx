7b5b9fc320022db5f3df94818ad1240eae3b4489
FalconeriLatrans <falconeri@ymail.com>
2026-06-11 14:50:27 -0300

    Mapa colorido

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,10 @@ import seats from "../data/seats.json";
 import Seat from "./Seat";
 
 export default function Map() {
+    
+    alert("Map executou");
+    console.log(seats[0]);
+
   const minX = Math.min(...seats.map((s) => s.x));
   const minY = Math.min(...seats.map((s) => s.y));
 
@@ -13,19 +17,20 @@ export default function Map() {
       className="map"
       style={{
         display: "grid",
-        gridTemplateColumns: `repeat(${maxX - minX + 3}, 70px)`,
-        gridTemplateRows: `repeat(${maxY - minY + 3}, 70px)`,
+        gridTemplateColumns: `repeat(${maxX - minX + 3}, 40px)`,
+        gridTemplateRows: `repeat(${maxY - minY + 3}, 40px)`,
+        backgroundColor: colors[category],
         gap: "4px",
         padding: "20px",
       }}
     >
       {seats.map((seat) => (
         <div
-          key={seat.seat}
-          style={{
-            gridColumn: seat.x - minX + 1,
-            gridRow: seat.y - minY + 1,
-          }}
+        key={seat.seat}
+        style={{
+          gridColumn: `${seat.x - minX + 1} / span ${seat.width}`,
+          gridRow: `${seat.y - minY + 1} / span ${seat.height}`,
+        }}
         >
           <Seat seat={seat.seat} />
         </div>
