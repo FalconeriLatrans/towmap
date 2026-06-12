13ec231d588a166624e649c171cf8ef7b5cae626
FalconeriLatrans <falconeri@ymail.com>
2026-06-12 08:52:52 -0300

    Ajustes Zoom

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -7,9 +7,9 @@ import {
 
 
 export default function Map() {
-    
-//    alert("Map executou");
-    console.log(seats[0]);
+
+  //    alert("Map executou");
+  console.log(seats[0]);
 
   const minX = Math.min(...seats.map((s) => s.x));
   const minY = Math.min(...seats.map((s) => s.y));
@@ -18,36 +18,51 @@ export default function Map() {
   const maxY = Math.max(...seats.map((s) => s.y));
 
   return (
-    <TransformWrapper
-    initialScale={0.3}
-    minScale={0.1}
-    maxScale={5}
-    centerOnInit
-  >
-    <TransformComponent>
     <div
-      className="map"
       style={{
-        display: "grid",
-        gridTemplateColumns: `repeat(${maxX - minX + 3}, 40px)`,
-        gridTemplateRows: `repeat(${maxY - minY + 3}, 40px)`,
-        gap: "4px",
-        padding: "20px",
+        width: "100vw",
+        height: "100vh",
+        background: "#d7ead3",
+        overflow: "hidden",
       }}
     >
-      {seats.map((seat) => (
-        <div
-        key={seat.seat}
-        style={{
-          gridColumn: `${seat.x - minX + 1} / span ${seat.width}`,
-          gridRow: `${seat.y - minY + 1} / span ${seat.height}`,
-        }}
-        >
-          <Seat seat={seat.seat} />
-        </div>
-      ))}
-    </div>
+      <TransformWrapper
+        initialScale={.5}
+        minScale={0.2}
+        maxScale={8}
+        centerOnInit
+        limitToBounds={false}
+        smooth={true}
+      >
+        <TransformComponent>
+          wrapperStyle={{
+            width: "100%",
+            height: "100%",
+          }}
+          <div
+            className="map"
+            style={{
+              display: "grid",
+              gridTemplateColumns: `repeat(${maxX - minX + 3}, 40px)`,
+              gridTemplateRows: `repeat(${maxY - minY + 3}, 40px)`,
+              gap: "4px",
+              padding: "20px",
+            }}
+          >
+            {seats.map((seat) => (
+              <div
+                key={seat.seat}
+                style={{
+                  gridColumn: `${seat.x - minX + 1} / span ${seat.width}`,
+                  gridRow: `${seat.y - minY + 1} / span ${seat.height}`,
+                }}
+              >
+                <Seat seat={seat.seat} />
+              </div>
+            ))}
+          </div>
         </TransformComponent>
-        </TransformWrapper>
+      </TransformWrapper>
+    </div>
   );
 }
