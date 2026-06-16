fdd2b265ef0b611784a21c4075cf219b83ed3b4a
null <null>
2026-06-16 08:01:56 -0300

    Firebase instalado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Map from "./components/Map/Map";
 import MapViewport from "./components/Map/MapViewport";
 import SearchPanel from "./components/SearchPanel/SearchPanel";
-import allocate from "./services/AllocationService";
+import { allocate } from "./services/AllocationService";
 import testFirestore from "./services/TestFirestore";
 
 export default function App() {
