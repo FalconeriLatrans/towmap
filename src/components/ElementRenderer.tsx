6810ae4f62151bfb8198b275d6802a6322a9b66b
null <null>
2026-07-14 07:31:52 -0300

    Readme updated

---
+++ src/components/ElementRenderer.tsx
@@ -0,0 +1,22 @@
+import Element from "./Elements/Element";
+//import Flag from "./Elements/Banner/Banner";
+
+export default function ElementRenderer({
+  element,
+  occupant,
+  selected,
+  editing,
+  onClick,
+}: any) {
+
+  return (
+    <Element
+      element={element}
+      occupant={occupant}
+      selected={selected}
+      editing={editing}
+      onClick={onClick}
+    />
+  );
+
+}
---
+++ src/components/MapElement.tsx
@@ -0,0 +1,9 @@
+export interface MapElement {
+    id: string;
+    label: string;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    type: string;
+  }
