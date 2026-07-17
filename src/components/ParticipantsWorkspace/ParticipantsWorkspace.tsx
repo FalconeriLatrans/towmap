065a58fd28c44212857f081b2c3e136897253e0f
null <null>
2026-07-17 16:16:52 -0300

    Criando ParticipantsWorkspace

--- src/components/ParticipantsWorkspace/ParticipantsWorkspace.tsx
+++ src/components/ParticipantsWorkspace/ParticipantsWorkspace.tsx
@@ -1,8 +1,33 @@
+import TopBar from "../TopBar/TopBar";
+import ParticipantsList from "./ParticipantsList/ParticipantsList";
+import ParticipantPropertiesPanel from "./ParticipantPropertiesPanel/ParticipantPropertiesPanel";
+import { useEffect, useState } from "react";
+import type { MapElement } from "./types/MapElement";
+import "./MapWorkspace.css";
+
 export default function ParticipantsWorkspace() {
 
+    const [filter, setFilter] = useState("");
+    const [participants, setParticipants] = useState<Participant[]>([]);
+    const [selectedParticipant, setSelectedParticipant] = useState<Participant | null>(null);
+
     return (
-        <div>
-            Participants Workspace
-        </div>
+
+        <>
+
+            <ParticipantsList
+                participants={participants}
+                filter={filter}
+                selectedParticipant={selectedParticipant}
+                setSelectedParticipant={setSelectedParticipant}
+            />
+
+            <ParticipantPropertiesPanel
+                participant={selectedParticipant}
+            />
+
+        </>
+
     );
+
 }
