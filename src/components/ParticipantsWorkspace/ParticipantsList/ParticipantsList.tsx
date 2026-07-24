d313afa0cff0931151c6f159089bf0202d7e89f4
null <null>
2026-07-24 16:12:49 -0300

    Inclusão de key no card do participante

--- src/components/ParticipantsWorkspace/ParticipantsList/ParticipantsList.tsx
+++ src/components/ParticipantsWorkspace/ParticipantsList/ParticipantsList.tsx
@@ -12,10 +12,34 @@ export default function ParticipantsList({
   selectedId,
   onSelect,
 }: Props) {
+
+/* LOG PARA VERIFICAR IDS ERRADOS NO FIRESTORE
+  console.log(
+    "Participants:",
+    participants.map((p) => ({
+      id: p.id,
+      name: p.name,
+    }))
+  );
+
+  const ids = participants.map((p) => p.id);
+
+console.log(
+  "Invalid IDs:",
+  participants.filter((p) => !p.id)
+);
+
+console.log(
+  "Duplicate IDs:",
+  ids.filter((id, index) => ids.indexOf(id) !== index)
+);
+*/
+
   return (
     <div className="participants-list">
       {participants.map(participant => (
         <button
+        key={participant.id}
           className={
             "participant-card" +
             (participant.id === selectedId ? " selected" : "")
